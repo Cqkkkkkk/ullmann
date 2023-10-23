7a974505dc6eb1ca089854da1d0823ee4629c7e5
chenqin <chenqink@outlook.com>
2023-10-23 11:41:06 +0000

    v3

--- graph.py
+++ graph.py
@@ -22,7 +22,7 @@ class Edge:
         self.end_vertex = end_vertex
 
     def __repr__(self):
-        return f"Edge(start={self.start_vertex.vid}, end={self.end_vertex.vid}, label={self.label})"
+        return f"Edge(start={self.start_vertex.vid}, end={self.end_vertex.vid})"
 
 
 
@@ -36,20 +36,11 @@ class Graph(object):
         self._adjacency_matrix = None
         self._adjacency_matrix = None
 
-    # def add_vertex(self, vid, label):
-    #     self.vertices.append(Vertex(int(vid), label))
     def add_vertex(self, vid, label):
         vertex = Vertex(int(vid), label)
         self.vertices.append(vertex)
         return vertex
 
-    # def add_edges(self, start, end, label):
-    #     start = int(start)
-    #     end = int(end)
-    #     self.edges.append(Edge(start, end, label))
-    #     self.vertices[start].add_neighbor(self.vertices[end])
-    #     self.edges.append(Edge(end, start, label))
-    #     self.vertices[end].add_neighbor(self.vertices[start])
     def add_edges(self, start_vid, end_vid, label):
         start_vertex = self.vertices[int(start_vid)]
         end_vertex = self.vertices[int(end_vid)]
@@ -61,15 +52,13 @@ class Graph(object):
         end_vertex.add_neighbor(start_vertex)
     
     def adjacency_matrix(self):
-        if self._adjacency_matrix is not None:
-            return self._adjacency_matrix
-        else:
+        if self._adjacency_matrix is None:
             size_t = len(self.vertices)
             self._adjacency_matrix = np.zeros((size_t, size_t))
-            for v_from in self.vertices:
-                for v_to in v_from.neighbors:
-                    self._adjacency_matrix[v_from.vid][v_to.vid] = 1
-            return self._adjacency_matrix
+            for edge in self.edges:
+                self._adjacency_matrix[edge.start_vertex.vid][edge.end_vertex.vid] = 1
+                self._adjacency_matrix[edge.end_vertex.vid][edge.start_vertex.vid] = 1            
+        return self._adjacency_matrix
 
     def show(self):
         for i in self.vertices:
