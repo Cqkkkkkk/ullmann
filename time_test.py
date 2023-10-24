35189b05b6505aa964dad10a69334e8b70d28b31
chenqin <chenqink@outlook.com>
2023-10-24 14:20:21 +0000

    add arg control

--- time_test.py
+++ time_test.py
@@ -2,26 +2,37 @@ import time, pdb, random
 import numpy as np
 from ullmann import Ullmann
 from utils import read_file
+import argparse
+
 
 if __name__ == '__main__':
 
-    test_name = 'Q4'
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--name', type=str)
+    arg_parser.add_argument('--repeat', type=int, default=1)
+    arg_parser.add_argument('--shuffle', action='store_true')
+    args = arg_parser.parse_args()
+
+    test_name = args.name
+    repeat = args.repeat
+
     graphs = read_file('./graphDB/mygraphdb.data')
     queries = read_file('./graphDB/{}.my'.format(test_name))
-
-    repeat = 5
+    
     num_graphs_used = 300
     num_queries_used = 300
+    print('Test on query set: ', test_name)
     print('Num graphs: ', num_graphs_used)
     print('Num queries:', num_queries_used)
     print('Num repeats:', repeat)
+    print('Shuffle:', args.shuffle)
 
     time_array = []
 
     for _ in range(repeat):
-
-        # random.shuffle(graphs)
-        # random.shuffle(queries)
+        if args.shuffle:
+            random.shuffle(graphs)
+            random.shuffle(queries)
         cur_graphs = graphs[:num_graphs_used]
         cur_queries = queries[:num_queries_used]
 
