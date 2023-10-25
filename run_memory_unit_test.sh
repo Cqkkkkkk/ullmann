f59e8ca8cf43d11531909c265b744a32892fb29d
chenqin <chenqink@outlook.com>
2023-10-25 08:38:56 +0000

    complete bash

--- run_memory_unit_test.sh
+++ run_memory_unit_test.sh
@@ -4,3 +4,4 @@ python memory_unit_test.py --name Q12
 python memory_unit_test.py --name Q16
 python memory_unit_test.py --name Q20
 python memory_unit_test.py --name Q24
+python memory_unit_analysis.py
--- run_time_unit_test.sh
+++ run_time_unit_test.sh
@@ -4,3 +4,4 @@ python time_unit_test.py --name Q12
 python time_unit_test.py --name Q16
 python time_unit_test.py --name Q20
 python time_unit_test.py --name Q24
+python time_unit_analysis.py
